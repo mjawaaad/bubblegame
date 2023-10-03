b7630c64740746825279d50883cc9614d302d7c7
Muhammad Jawad <mjawad.business@gmail.com>
2023-10-03 13:01:46 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var clutter = "";
+
+for(var i = 1; i<=102; i++){
+    var rn = Math.floor(Math.random()*10);
+    clutter += ` <div class="bubble">${rn}</div>`;
+}
+
+document.querySelector("#pbtm").innerHTML = clutter;
